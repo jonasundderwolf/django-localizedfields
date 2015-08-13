a0e8c92a9d12846c8cfe6819ea26d1e08dd4098a
andreas <andreas.rieger@gmail.com>
2015-08-13 17:05:46 +0200

    Make fields in example app non required

--- example/models.py
+++ example/models.py
@@ -1,20 +1,22 @@
+from django.db import models
 import i18n
 from i18n.models import TranslatableModel
 
 
 class Document(TranslatableModel):
-
+    untranslated_charfield = models.CharField(max_length=50, blank=True)
     charfield = i18n.LocalizedCharField(max_length=50)
-    textfield = i18n.LocalizedTextField(max_length=512)
-    filefield = i18n.LocalizedFileField(null=True, upload_to='files')
-    imagefield = i18n.LocalizedImageField(null=True, upload_to='images')
+    textfield = i18n.LocalizedTextField(max_length=500, blank=True)
+    filefield = i18n.LocalizedFileField(null=True, upload_to='files', blank=True)
+    imagefield = i18n.LocalizedImageField(null=True, upload_to='images', blank=True)
     booleanfield = i18n.LocalizedBooleanField()
-    datefield = i18n.LocalizedDateField()
+    datefield = i18n.LocalizedDateField(blank=True, null=True)
     fkfield = i18n.LocalizedForeignKey('self', null=True, blank=True,
                                        related_name='+')
-    urlfied = i18n.LocalizedURLField()
-    decimalfield = i18n.LocalizedDecimalField(max_digits=4, decimal_places=2)
-    integerfield = i18n.LocalizedIntegerField()
+    urlfied = i18n.LocalizedURLField(null=True, blank=True)
+    decimalfield = i18n.LocalizedDecimalField(max_digits=4, decimal_places=2, null=True,
+                                              blank=True)
+    integerfield = i18n.LocalizedIntegerField(null=True, blank=True)
 
     def __str__(self):
         return '%d, %s' % (self.pk, self.charfield)
