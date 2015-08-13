1a116cc4bc8e3f9f3f5a2235de2d52e3c0c7c97d
andreas <andreas.rieger@gmail.com>
2015-08-13 17:04:27 +0200

    Add middleware classes for Django 1.8 in example project

--- example/settings.py
+++ example/settings.py
@@ -50,3 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'example',
 ]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
