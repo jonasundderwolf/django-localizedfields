63a20a96f976ee8155587c7d0e6ae3ba5daf1754
Sophie <sophie.wirth@jonasundderwolf.de>
2017-11-23 16:01:15 +0000

    Bump Version to 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="django-localizedfields",
     author="Jonas und der Wolf GmbH",
     author_email="info@jonasundderwolf.de",
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=('django-composite-field==0.3'),
     include_package_data=True,
