4e2612aaaf81041de9569e59d20163f8f810071b
Matthieu Rigal <matthieu.rigal@jonasundderwolf.de>
2020-09-22 16:52:43 +0200

    Fix example urls to work with recent Django

--- example/urls.py
+++ example/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.conf.urls import include, patterns, static, url
+from django.conf.urls import static
 from django.contrib import admin
+from django.urls import path
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    "",
-    url(r"^admin/", include(admin.site.urls)),
-)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
 urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
